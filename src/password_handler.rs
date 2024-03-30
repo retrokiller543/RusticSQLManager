d5c9e9f693890e660bcbc73440efd0491034952b
Retrokiller543 <emil.schutt@gmail.com>
2024-03-30 13:08:34 +0100

    patched issue where keyring was required

--- src/password_handler.rs
+++ src/password_handler.rs
@@ -2,35 +2,48 @@ use anyhow::Result;
 use keyring::Entry;
 use std::io;
 
+fn get_keyring(usr: &str, host: &str) -> Result<Entry> {
+    Ok(Entry::new("rustic_sql_manager", format!("{usr}@{host}").as_str())?)
+} 
+
 pub fn get_db_pass(usr: &str, host: &str) -> Result<String> {
-    let entry = Entry::new("rustic_sql_manager", format!("{usr}@{host}").as_str())?;
+    match get_keyring(usr, host) {
+        Ok(entry) => {
+            match entry.get_password() {
+                Ok(pass) => Ok(pass),
+                Err(_) => {
+                    let pass = rpassword::prompt_password("Enter User password: ")?;
+                    // does the user want to save the password? y/n
+                    loop {
+                        println!("Do you want to save your password? (yes|Y|y/no|N|n)");
+                        let mut save_password = String::new();
+                        io::stdin()
+                            .read_line(&mut save_password)
+                            .expect("Failed to read line");
+                        let save_password = save_password.trim().to_lowercase();
 
-    match entry.get_password() {
-        Ok(pass) => Ok(pass),
-        Err(_) => {
-            let pass = rpassword::prompt_password("Enter password: ")?;
-            // does the user want to save the password? y/n
-            loop {
-                println!("Do you want to save your password? (yes|Y|y/no|N|n)");
-                let mut save_password = String::new();
-                io::stdin()
-                    .read_line(&mut save_password)
-                    .expect("Failed to read line");
-                let save_password = save_password.trim().to_lowercase();
-
-                match save_password.as_str() {
-                    "yes" | "y" | "Y" => {
-                        println!("Saving password...");
-                        entry.set_password(&pass)?;
-                        break;
+                        match save_password.as_str() {
+                            "yes" | "y" => {
+                                println!("Saving password...");
+                                if entry.set_password(&pass).is_err() {
+                                    eprintln!("No Native Keyring found. Please install one and try again.");
+                                }
+                                break;
+                            }
+                            "no" | "n" => {
+                                println!("Not saving password.");
+                                break;
+                            }
+                            _ => println!("Invalid input. Please enter 'yes' or 'no'."),
+                        }
                     }
-                    "no" | "n" | "N" => {
-                        println!("Not saving password.");
-                        break;
-                    }
-                    _ => println!("Invalid input. Please enter 'yes' or 'no'."),
+                    Ok(pass)
                 }
             }
+        }
+        Err(_) => {
+            eprintln!("No Native Keyring found. Please install one and try again.");
+            let pass = rpassword::prompt_password("Enter User password: ")?;
             Ok(pass)
         }
     }
